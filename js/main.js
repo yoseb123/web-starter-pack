ba472f4d0b0c61b952eb618108cd696cd345b11f
austindg <austindg@uw.edu>
2015-12-11 12:26:31 -0800

    Tabs to spaces

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var React = require('react'),
-	ReactDOM = require('react-dom'),
+    ReactDOM = require('react-dom'),
     constants = require('./lib/constant');
 
 console.log('JavaScript Working');
